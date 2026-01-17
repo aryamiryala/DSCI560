cbe5e6dd6ae4a4aa6274b239f875b54c411e60df
Arya Miryala <aryamiryala5@gmail.com>
2026-01-17 01:48:50 -0800

    added comments

--- AryaMiryala_9567678120/scripts/web_scraper.py
+++ AryaMiryala_9567678120/scripts/web_scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+#target url for the scraping task
 url = "https://www.cnbc.com/world/?region=world"
 
 headers = {
@@ -9,12 +10,17 @@ headers = {
 
 }
 
+#send the get request to the url using headers
 response = requests.get(url, headers=headers)
 
+#make sure request was successful
 if response.status_code == 200:
+	#parse the html content
 	soup = BeautifulSoup(response.content, 'html.parser')
+	#define the file path
 	file_path = os.path.join("..", "data", "raw_data", "web_data.html")
-
+	
+	#open the file for writing
 	with open(file_path, "w", encoding = "utf-8") as f: 
 		f.write(str(soup))
 
